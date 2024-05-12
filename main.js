8b9b057dfdbc4ef2195d694c063e1343fa59cae3
HughCodeX <2226518459@qq.com>
2024-05-12 21:38:09 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function deepClone(target, map = new WeakMap()) {
+    if (typeof target === 'object') {
+        let cloneTarget = Array.isArray(target) ? [] : {};
+        if (map.get(target)) {
+            return map.get(target);
+        }
+        map.set(target, cloneTarget);
+        for (const key in target) {
+            cloneTarget[key] = deepClone(target[key], map);
+        }
+        return cloneTarget;
+    } else {
+        return target;
+    }
+};
+
+const target = {
+    field1: 1,
+    field2: undefined,
+    field3: {
+        child: 'child'
+    },
+    field4: [2, 4, 8]
+};
+target.target = target;
+
+let cloneObj = deepClone(target);
+console.log(cloneObj);
